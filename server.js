1523f86008ee9b0794629a2a0013529c8e421271
Aaron Chris <rooneychris83@gmail.com>
2022-12-11 18:07:54 +0100

    added vercel

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { handler  as ssrHandler } from './dist/server/entry.mjs';
+
+
+
+
+const app = express();
+app.use(express.static('dist/client'))
+app.use(express.static('dist/client/raw'))
+app.use(ssrHandler)
