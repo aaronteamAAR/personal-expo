64df8c37113b162b9b138d8ab11b56069f169875
Aaron Chris <rooneychris83@gmail.com>
2022-12-11 21:40:27 +0100

    fixed header title

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
 
@@ -14,5 +15,7 @@ export default defineConfig({
           langs: ['python'],
         },
       },
-    integrations: [react(), mdx()],
+    integrations: [react(), mdx(), tailwind({
+      plugins: [require("daisyui")],
+    })],
 });
