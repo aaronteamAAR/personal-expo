d970439946dfa31984cae5a8957a660df6ef0506
Aaron Chris <rooneychris83@gmail.com>
2022-12-11 16:27:59 +0100

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
 
 
 // https://astro.build/config
 export default defineConfig({
-
     output: 'server',
+    adapter: vercel(),
     markdown: {
         shikiConfig: {
           theme: 'dark-plus',
